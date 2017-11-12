c08769f0f5091699fd476b21260c3e5c1a52a31c
osemcru <cruzsuazocristianandres@gmail.com>
2017-11-12 16:31:30 -0500

    add

--- ProyectoJuego/src/Vista/FrmInicioJuego.java
+++ ProyectoJuego/src/Vista/FrmInicioJuego.java
@@ -48,7 +48,7 @@ public class FrmInicioJuego extends javax.swing.JFrame {
         LbPregunta.setForeground(new java.awt.Color(255, 255, 255));
         LbPregunta.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(255, 204, 0)));
         LbPregunta.setOpaque(true);
-        getContentPane().add(LbPregunta, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 90, 400, 90));
+        getContentPane().add(LbPregunta, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 80, 400, 90));
 
         Chb1.setBackground(new java.awt.Color(0, 0, 0));
         Chb1.setFont(new java.awt.Font("Times New Roman", 3, 14)); // NOI18N
@@ -58,7 +58,7 @@ public class FrmInicioJuego extends javax.swing.JFrame {
                 Chb1ActionPerformed(evt);
             }
         });
-        getContentPane().add(Chb1, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 240, 60, 30));
+        getContentPane().add(Chb1, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 230, 60, 30));
 
         Chb2.setBackground(new java.awt.Color(0, 0, 0));
         Chb2.setFont(new java.awt.Font("Times New Roman", 3, 14)); // NOI18N
@@ -68,31 +68,32 @@ public class FrmInicioJuego extends javax.swing.JFrame {
                 Chb2ActionPerformed(evt);
             }
         });
-        getContentPane().add(Chb2, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 270, 60, 30));
+        getContentPane().add(Chb2, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 260, 60, 30));
 
         Chb3.setBackground(new java.awt.Color(0, 0, 0));
         Chb3.setFont(new java.awt.Font("Times New Roman", 3, 14)); // NOI18N
         Chb3.setForeground(new java.awt.Color(255, 255, 255));
-        getContentPane().add(Chb3, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 300, 60, 30));
+        getContentPane().add(Chb3, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 290, 60, 30));
 
         Chb4.setBackground(new java.awt.Color(0, 0, 0));
         Chb4.setFont(new java.awt.Font("Times New Roman", 3, 14)); // NOI18N
         Chb4.setForeground(new java.awt.Color(255, 255, 255));
-        getContentPane().add(Chb4, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 330, 60, 30));
+        getContentPane().add(Chb4, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 320, 60, 30));
 
         BtnSiguiente.setFont(new java.awt.Font("Times New Roman", 3, 18)); // NOI18N
         BtnSiguiente.setForeground(new java.awt.Color(255, 0, 0));
         BtnSiguiente.setText("SIGUIENTE");
-        getContentPane().add(BtnSiguiente, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 380, -1, 32));
+        getContentPane().add(BtnSiguiente, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 370, -1, 32));
 
         LbCategoria.setBackground(new java.awt.Color(0, 0, 0));
         LbCategoria.setText("jLabel2");
         LbCategoria.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(255, 204, 0)));
         LbCategoria.setOpaque(true);
-        getContentPane().add(LbCategoria, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 30, 220, 30));
+        getContentPane().add(LbCategoria, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 20, 220, 30));
 
+        jLabel1.setBackground(new java.awt.Color(0, 0, 0));
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/light-1443836_960_720.jpg"))); // NOI18N
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(-140, 0, 800, 450));
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(-100, -80, 760, 500));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
