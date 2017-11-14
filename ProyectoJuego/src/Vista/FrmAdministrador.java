2ca6e51ff115f5af3211bc6db941055fc7037aec
xgioserx <xgioserx@gmail.com>
2017-11-14 05:32:57 -0500

    Report1

--- ProyectoJuego/src/Vista/FrmAdministrador.java
+++ ProyectoJuego/src/Vista/FrmAdministrador.java
@@ -266,7 +266,7 @@ public class FrmAdministrador extends javax.swing.JFrame {
         try {
 
                     conn.conectar();
-                    String dir = "C:\\GitHub\\Proyecto-Desarrollo\\ProyectoJuego\\src\\Reportes\\EstPorc.jrxml";
+                    String dir = "C:\\GitHub\\Proyecto-Desarrollo\\ProyectoJuego\\src\\Reportes\\EstuPorc.jrxml";
                     JasperReport reporteJasper = JasperCompileManager.compileReport(dir);
                     JasperPrint mostrarReporte = JasperFillManager.fillReport(reporteJasper, null, conn.getConn());
                     JasperViewer.viewReport(mostrarReporte);
